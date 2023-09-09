a997d1869233ed05de207ba3655164ff09a283d7
Gisang Lee <masterkorea01@naver.com>
2023-09-09 19:35:55 +0900

    version setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='python-tosspayments',
-    version='0.0.4',
+    version='0.0.5',
     description='Toss Payments SDK for python',
     author='jsonlee',
     author_email='dudegs.py@gmail.com',
