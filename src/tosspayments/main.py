4cb6bc5feec78eeada6057d7d3792d7fa8456e9e
Gisang Lee <masterkorea01@naver.com>
2023-09-09 22:19:23 +0900

    get payment

--- src/tosspayments/main.py
+++ src/tosspayments/main.py
@@ -7,7 +7,9 @@ class TossPayments(object):
         # self.__secret_key = settings.TOSS_PAYMENT_SECRET
         self.__base_url = "https://api.tosspayments.com/"
         self.__secret_key = secret_key
-        self.__api_url = "v1/payments/"
+        self.api_version = "v1"
+        self.__payment_api_url = f"{self.api_version}/payments/"
+        self.__transactions_api_url = f"{self.api_version}/transactions/"
 
     class HttpError(Exception):
         def __init__(self, code: str, error_type: str, reason=None):
@@ -69,7 +71,7 @@ class TossPayments(object):
 
     def confirm(self, payment_key: str, order_id: str, amount: int, idempotency_key: str = None):
         response = self.__post(
-            "".join([self.__base_url, self.__api_url, "confirm"]),
+            "".join([self.__base_url, self.__payment_api_url, "confirm"]),
             data={
                 "paymentKey": payment_key,
                 "orderId": order_id,
@@ -87,20 +89,18 @@ class TossPayments(object):
         # virtual_account refund info
         refund_receive_account = cancel_data.get("refund_receive_account", {})
 
+        data = {"cancelReason": cancel_data["cancel_reason"]}
+
         if refund_receive_account:
-            data = {
-                "cancelReason": cancel_data["cancel_reason"],
-                "refundReceiveAccount": {
-                    "accountNumber": refund_receive_account.get("account_number"),
-                    "bank": refund_receive_account.get("bank"),
-                    "holderName": refund_receive_account.get("holder_name"),
-                },
+            data["refundReceiveAccount"] = {
+                "accountNumber":
+                    refund_receive_account.get("account_number"),
+                "bank": refund_receive_account.get("bank"),
+                "holderName": refund_receive_account.get("holder_name"),
             }
-        else:
-            data = {"cancelReason": cancel_data["cancel_reason"]}
 
         response = self.__post(
-            "".join([self.__base_url, self.__api_url, f"{payment_key}/", "cancel"]),
+            "".join([self.__base_url, self.__payment_api_url, f"{payment_key}/", "cancel"]),
             data=data,
             idempotency_key=idempotency_key,
         )
@@ -110,10 +110,40 @@ class TossPayments(object):
         else:
             return self.__get_response(response)
 
-    def search_payment(self, payment_key: str):
+    def get_payment_by_payment_key(self, payment_key: str):
         response = self.__get(
-            "".join([self.__base_url, self.__api_url, f"{payment_key}"]),
+            "".join([self.__base_url, self.__payment_api_url, f"{payment_key}"]),
             params={"paymentKey": payment_key},
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return self.__get_response(response)
+
+    def get_payment_by_order_id(self, order_id: str):
+        response = self.__get(
+            "".join([self.__base_url, self.__payment_api_url, "orders/", f"{order_id}"]),
+            params={"orderId": order_id},
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return self.__get_response(response)
+
+    def get_transaction(self, start_date: str, end_date: str, starting_after: str = None, limit: int = None):
+        # 파라미터를 추가할 리스트 생성
+        potential_params = [
+            ("startDate", start_date),
+            ("endDate", end_date),
+            ("startingAfter", starting_after),
+            ("limit", limit)
+        ]
+
+        response = self.__get(
+            "".join([self.__base_url, self.__transactions_api_url]),
+            params={key: value for key, value in potential_params if value},  # None이 아닌 값만 딕셔너리에 추가
         )
 
         if response.status_code == 200:
