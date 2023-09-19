6ee855727be2bf16c79a5dac6c2d3051b336bb9e
GisangLee <masterkorea01@naver.com>
2023-09-19 12:37:01 +0900

    doc string and payment cancel

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='python-tosspayments',
-    version='0.1.1',
-    description='Toss Payments SDK for python. 토스페이먼츠가 아닌 제 3자가 만든 SDK입니다.',
-    author='jsonlee',
-    author_email='dudegs.py@gmail.com',
+    name="python-tosspayments",
+    version="1.0.1",
+    description="Toss Payments SDK for python. 토스페이먼츠가 아닌 제 3자가 만든 SDK입니다.",
+    author="jsonlee",
+    author_email="dudegs.py@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/GisangLee/python-tosspayment',
+    url="https://github.com/GisangLee/python-tosspayment",
     license="MIT",
     install_requires=["requests"],
-    keywords=['toss', 'toss payment', 'payment'],
-    python_requires='>=3.8',
+    keywords=["toss", "toss payment", "payment"],
+    python_requires=">=3.8",
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
